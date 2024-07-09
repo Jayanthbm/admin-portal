39138f01b541183ba3e561bcf5a77a3073bdcdcc
Jayanthbharadwaj M <jayanthbharadwajm@gmail.com>
2024-07-09 13:11:30 +0530

    minor updates

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
 import AppHeader from "./components/appHeader";
+import { PATHS } from "./constants";
 import DashboardScreen from "./pages/DashboardScreen";
 import DoctorsScreen from "./pages/DoctorsScreen";
 import HomeScreen from "./pages/HomeScreen";
@@ -9,29 +10,18 @@ import LoginScreen from "./pages/LoginScreen";
 import SpecialitiesScreen from "./pages/SpecialitiesScreen";
 import UserRecordConfigScreen from "./pages/UserRecordConfigScreen";
 
-const Checker = () => {
-  const navigate = useNavigate();
-  useEffect(() => {
-    navigate("/admin-portal");
-  });
-  return <div>Checker</div>;
-};
 const App = () => {
   return (
     <>
       <AppHeader />
       <Routes>
-        <Route path="/" element={<Checker />} />
-        <Route path="/admin-portal" element={<HomeScreen />} />
-        <Route path="/admin-portal/login" element={<LoginScreen />} />
-        <Route path="/admin-portal/dashboard" element={<DashboardScreen />} />
-        <Route path="/admin-portal/doctors" element={<DoctorsScreen />} />
+        <Route path={PATHS.ROOT} element={<HomeScreen />} />
+        <Route path={PATHS.LOGIN} element={<LoginScreen />} />
+        <Route path={PATHS.DASHBOARD} element={<DashboardScreen />} />
+        <Route path={PATHS.DOCTORS} element={<DoctorsScreen />} />
+        <Route path={PATHS.SPECIALITIES} element={<SpecialitiesScreen />} />
         <Route
-          path="/admin-portal/specialities"
-          element={<SpecialitiesScreen />}
-        />
-        <Route
-          path="/admin-portal/user-record-config"
+          path={PATHS.USER_RECORD_CONFIG}
           element={<UserRecordConfigScreen />}
         />
       </Routes>
