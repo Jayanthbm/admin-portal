b2e5c6328964ab3634070640f6a62c0731d5d146
Jayanthbharadwaj M <jayanthbharadwajm@gmail.com>
2024-07-12 16:14:20 +0530

    custom modal updated

--- src/components/MyModal.js
+++ src/components/MyModal.js
@@ -1,9 +1,26 @@
 // src/components/MyModal.js
 
-import { Box, Modal } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import { Box, Button, Modal, Typography } from "@mui/material";
 import React from "react";
 
-const MyModal = ({ children, open, handleClose }) => {
+const MyModal = ({
+  children,
+  open,
+  handleClose,
+  title,
+  titleVariant = "h6",
+  subTitle,
+  okButtonText,
+  okButtonColor = "primary",
+  okButtonIcon,
+  okButtondisabled = false,
+  cancelButtonText,
+  onOk,
+  onCancel,
+  cancelButtonColor = "primary",
+  isLoading = false,
+}) => {
   return (
     <Modal open={open} onClose={handleClose}>
       <Box
@@ -19,7 +36,34 @@ const MyModal = ({ children, open, handleClose }) => {
           p: 4,
         }}
       >
-        {children}
+        <>
+          <Typography variant={titleVariant} gutterBottom>
+            {title}
+          </Typography>
+          {subTitle && <Typography variant="body2">{subTitle}</Typography>}
+          {children}
+          <Box mt={2} display="flex" justifyContent="space-between">
+            <Button
+              onClick={onCancel}
+              variant="outlined"
+              color={cancelButtonColor}
+              disabled={isLoading}
+            >
+              {cancelButtonText}
+            </Button>
+            <LoadingButton
+              onClick={onOk}
+              variant="contained"
+              color={okButtonColor}
+              loading={isLoading}
+              loadingPosition="start"
+              startIcon={okButtonIcon}
+              disabled={okButtondisabled}
+            >
+              {okButtonText}
+            </LoadingButton>
+          </Box>
+        </>
       </Box>
     </Modal>
   );
