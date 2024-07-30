571b5696e5a3dd481dea239cee3c40ff2f040014
Jayanthbharadwaj M <jayanthbharadwajm@gmail.com>
2024-07-30 17:29:08 +0530

    API url updated

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 // src/constants.js
 
-export const BASE_URL = 'https://c01b-103-186-41-217.ngrok-free.app/api';
+export const BASE_URL = 'https://db90-103-186-41-217.ngrok-free.app/api';
 
 export const TOKEN_KEY = 'accessToken';
 export const REFRESH_KEY = 'refreshToken';
